fb2f4c56ea6be59a873928a01cd8e52f0fe379ff
Hurricane <admin@dyn-129-236-214-159.dyn.columbia.edu>
2022-11-14 15:31:56 -0500

    requirement.txt

--- soccerFans/server.py
+++ soccerFans/server.py
@@ -214,5 +214,33 @@ def user():
     return render_template('home.html')
 
 
-if __name__ == '__main__':
-    server.run()
+# if __name__ == '__main__':
+#     server.run()
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+
+        python server.py
+
+    Show the help text using
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    server.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+
+
+  run()
